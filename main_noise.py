6124bcad28576a7331ea35be9fdb2e2881e73d36
lumimim <41974416+lumimim@users.noreply.github.com>
2023-10-11 21:17:01 -0700

    Update main_noise.py

--- main_noise.py
+++ main_noise.py
@@ -237,7 +237,7 @@ if __name__ == '__main__':
     parser.add_argument('--sample_times', type=int, default=32, help='sample times')
     
     args = parser.parse_args()
-    from models.model_noise_last1 import *
+    from models.model_noise import *
 
     tl.global_flag['mode'] = args.mode
 
