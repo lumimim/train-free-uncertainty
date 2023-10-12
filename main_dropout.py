7d5d3893b723bc88168828bcacfe90a56116f5a4
lumimim <41974416+lumimim@users.noreply.github.com>
2023-10-11 20:56:42 -0700

    Update main_dropout.py

--- main_dropout.py
+++ main_dropout.py
@@ -256,16 +256,16 @@ if __name__ == '__main__':
     parser.add_argument('--drop_out_keep', type=float, default=0.5, help='sample numbers')
     args = parser.parse_args()
     
-    if args.layer ==4:
-        from model_dropout_last4 import *
+    if args.layer ==1:
+        from models.model_dropout_last1 import *
     elif args.layer ==2:
-        from model_dropout_last2 import *
+        from models.model_dropout_last2 import *
+    elif args.layer ==4:
+        from models.model_dropout_last4 import *
     elif args.layer ==8:
-        from model_dropout_last8 import *
-    elif args.layer ==1:
-        from model_dropout_last1 import *
+        from models.model_dropout_last8 import *
     elif args.layer ==9:
-        from model_dropout_last9 import *
+        from models.model_dropout_last9 import *
     tl.global_flag['mode'] = args.mode
 
     if tl.global_flag['mode'] == 'srgan':
